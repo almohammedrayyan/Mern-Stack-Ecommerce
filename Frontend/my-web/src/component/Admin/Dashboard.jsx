3184823fdcb92864d8fb9456b836ec34490815b9
almohammedrayyan <almohammedrayyan@gmail.com>
2023-02-25 07:47:15 +0530

    updated.

--- Frontend/my-web/src/component/Admin/Dashboard.jsx
+++ Frontend/my-web/src/component/Admin/Dashboard.jsx
@@ -1,7 +1,32 @@
 import React from "react";
-
+import Chart from "../Chart/Chart";
+import Featured from "../Featured/Featured";
+import Sidebar from "../Sidebar/Sidebar";
+import Widgets from "../Widgets/Widget";
+import "./dashboard.css";
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        {/* <Navbar /> */}
+        <div className="widgets">
+          <Widgets type="user" />
+          <Widgets type="order" />
+          <Widgets type="earning" />
+          <Widgets type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
+        {/* <div className="listContianer">
+      <div className="listTitle">Latest Transaction</div>
+      <Tables />
+    </div> */}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ Frontend/my-web/src/component/Sidebar/Sidebar.jsx
@@ -0,0 +1,61 @@
+import React, { useContext } from "react";
+import DashboardCustomizeOutlinedIcon from "@mui/icons-material/DashboardCustomizeOutlined";
+import "./sidebar.scss";
+import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
+import ReorderIcon from "@mui/icons-material/Reorder";
+
+import { Link } from "react-router-dom";
+const Sidebar = () => {
+  // const { dispatch } = useContext(DarkModeContext);
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">Crusion</span>
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <Link to="/admin/dashboard" style={{ textDecoration: "none" }}>
+            <li>
+              <DashboardCustomizeOutlinedIcon className="icon" />
+              <span>Dashboard</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <Inventory2OutlinedIcon className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
+          <Link to="/review" style={{ textDecoration: "none" }}>
+            <li>
+              <Inventory2OutlinedIcon className="icon" />
+              <span>Review</span>
+            </li>
+          </Link>
+
+          <li>
+            <ReorderIcon className="icon" />
+            <span>Orders</span>
+          </li>
+        </ul>
+      </div>
+
+      {/* <div className="bottom">
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "LIGHT" })}
+        ></div>
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "DARK" })}
+        ></div>
+      </div> */}
+    </div>
+  );
+};
+
+export default Sidebar;
